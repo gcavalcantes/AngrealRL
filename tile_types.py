c799333e38e7d03582ed2becfd8d2daa3106ee9a
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2021-09-30 21:54:34 -0300

    FOV
    Added fully funcional FOV to the game.

--- tile_types.py
+++ tile_types.py
@@ -12,9 +12,10 @@ graphic_dt = np.dtype(
 
 tile_dt = np.dtype(
     [
-        ("walkable", np.bool_),
-        ("transparent", np.bool_),
-        ("dark", graphic_dt),
+        ("walkable", np.bool_), # Graphics for when the tile can be walked over
+        ("transparent", np.bool_), # True if the tile does not block FOV
+        ("dark", graphic_dt), # Graphics for when the tile
+        ("light", graphic_dt), # Graphics for when the tile
     ]
 )
 
@@ -23,15 +24,25 @@ def new_tile(
     walkable: int,
     transparent: int,
     dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
+    light: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
 ) -> np.ndarray:
     """
     Helper function for defining the individual tile types
     """
-    return np.array((walkable, transparent, dark), dtype=tile_dt)
+    return np.array((walkable, transparent, dark, light), dtype=tile_dt)
+
+# SHROUD represents unexplored, unseen tiles
+SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
 
 floor = new_tile(
-    walkable=True, transparent=True, dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
+    walkable=True, 
+    transparent=True, 
+    dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
+    light=(ord(" "), (255, 255, 255), (200, 180, 50)),
 )
 wall = new_tile(
-    walkable=False, transparent=False, dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
+    walkable=False, 
+    transparent=False, 
+    dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
+    light=(ord(" "), (255, 255, 255), (130, 110, 50)),
 )
