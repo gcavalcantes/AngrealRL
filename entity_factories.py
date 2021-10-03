2790d3404c863b47e4f4e1fa3d93dd72980049a5
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2021-10-03 17:04:40 -0300

    Enemies
    The now spawn enemies. 0 to 2 for each room. They have no AI yet.

---
+++ entity_factories.py
@@ -0,0 +1,6 @@
+from entity import T, Entity
+
+player = Entity(char="@", color=(255, 255, 255), name="Player", block_movement=True)
+
+orc = Entity(char="o", color=(63, 127, 63), name="Orc", block_movement=True)
+troll = Entity(char="T", color=(0, 127, 0), name="Troll", block_movement=True)
