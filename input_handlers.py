6ee4cd3ac538f1d068985ea2faf49960826eeead
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2021-10-12 19:14:11 -0300

    Update a lot
    Update TCOD to the latest version.
    Changed a lot of things. The game remains the same.
    Tests ran fine. Onto the next step.

--- input_handlers.py
+++ input_handlers.py
@@ -1,12 +1,32 @@
+from __future__ import annotations
+
 # Python's type hinting system. Optional denotes something that could be set to none.
-from typing import Optional
+from typing import Optional, TYPE_CHECKING
 
 import tcod.event
 
 from actions import Action, BumpAction, EscapeAction
 
+if TYPE_CHECKING:
+    from engine import Engine
+
 # Imports the action class and its subclasses
 class EventHandler(tcod.event.EventDispatch[Action]):
+    def __init__(self, engine: Engine):
+        self.engine = engine
+
+    def handle_events(self) -> None:
+        for event in tcod.event.wait():
+            action = self.dispatch(event)
+
+            if action is None:
+                continue
+
+            action.perform()
+
+            self.engine.handle_enemy_turns()
+            self.engine.update_fov() # Update the FOV before the players next action.
+
     def ev_quit(self, event: "tcod.event.Quit") -> Optional[Action]:
         raise SystemExit()
 
@@ -15,17 +35,19 @@ class EventHandler(tcod.event.EventDispatch[Action]):
 
         key = event.sym
 
+        player = self.engine.player
+
         if key == tcod.event.K_UP:
-            action = BumpAction(dx=0, dy=-1)
+            action = BumpAction(player, dx=0, dy=-1)
         elif key == tcod.event.K_DOWN:
-            action = BumpAction(dx=0, dy=1)
+            action = BumpAction(player, dx=0, dy=1)
         elif key == tcod.event.K_LEFT:
-            action = BumpAction(dx=-1, dy=0)
+            action = BumpAction(player, dx=-1, dy=0)
         elif key == tcod.event.K_RIGHT:
-            action = BumpAction(dx=1, dy=0)
+            action = BumpAction(player, dx=1, dy=0)
         
         elif key == tcod.event.K_ESCAPE:
-            action = EscapeAction()
+            action = EscapeAction(player)
         
         # When no valid key is pressed
         return action
