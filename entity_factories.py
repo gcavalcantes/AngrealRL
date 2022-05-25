7bbb5ec48928c9b88ac039e397f47f5a28c18f6b
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2022-05-25 15:08:48 -0300

    Final rework
    Project restructure is done.
    Everything is ready to the continued development.

--- entity_factories.py
+++ entity_factories.py
@@ -1,5 +1,6 @@
 from components.ai import HostileEnemy
 from components import consumable, equippable
+from components.equipment import Equipament
 from components.fighter import Fighter
 from components.inventory import Inventory
 from components.level import Level
@@ -10,7 +11,8 @@ player = Actor(
     color=(255, 255, 255),
     name="Player",
     ai_cls=HostileEnemy,
-    fighter=Fighter(hp=30, defense=2, power=5),
+    equipment=Equipament(),
+    fighter=Fighter(hp=30, base_defense=1, base_power=2),
     inventory=Inventory(capacity=26),
     level=Level(level_up_base=200),
 )
@@ -20,7 +22,8 @@ orc = Actor(
     color=(63, 127, 63),
     name="Orc",
     ai_cls=HostileEnemy,
-    fighter=Fighter(hp=10, defense=0, power=3),
+    equipment=Equipament(),
+    fighter=Fighter(hp=10, base_defense=0, base_power=3),
     inventory=Inventory(capacity=0),
     level=Level(xp_given=35),
 )
@@ -30,7 +33,8 @@ troll = Actor(
     color=(0, 127, 0),
     name="Troll",
     ai_cls=HostileEnemy,
-    fighter=Fighter(hp=16, defense=1, power=4),
+    equipment=Equipament(),
+    fighter=Fighter(hp=16, base_defense=1, base_power=4),
     inventory=Inventory(capacity=0),
     level=Level(xp_given=100),
 )
